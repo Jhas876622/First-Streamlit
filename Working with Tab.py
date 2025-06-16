3ed342d9c45ae7524adcbbfd2ddf80df39215418
Satyam Jha <158205679+Jhas876622@users.noreply.github.com>
2025-06-16 18:14:04 +0530

    Working with Tab.py

--- Working with Tab.py
+++ Working with Tab.py
@@ -1,20 +1,19 @@
-import time
 import numpy as np
 import pandas as pd
 import streamlit as st
 
+# Static Tabs
+tab1, tab2, tab3 = st.tabs(['Cat', 'Dog', 'Owl'])
+tab1.image('https://static.streamlit.io/examples/cat.jpg', width=200)
+tab2.image('https://static.streamlit.io/examples/dog.jpg', width=200)
+tab3.image('https://static.streamlit.io/examples/owl.jpg', width=200)
 
-tab1, tab2 , tab3 = st.tabs(['Cat', 'Dog','Owl'])
-tab1.image('https://static.streamlit.io/examples/cat.jpg', width = 200)
-tab2.image('https://static.streamlit.io/examples/dog.jpg', width = 200)
-tab3.image('https://static.streamlit.io/examples/owl.jpg', width = 200)
+# Upload CSV with Image URLs
+uploaded_file = st.file_uploader("Upload a CSV with image links (column: img_link)", type="csv")
+if uploaded_file is not None:
+    imgs = pd.read_csv(uploaded_file)['img_link']
 
-# Dynamic
-
-imgs = pd.read_csv(r'C:\Users\Satyam Jha\Desktop\DATA SCIENCE\STREAMLIT\imgs.csv')['img_link']
-
-
-tabs = st.tabs(["ID"]*30)
-for tab in tabs:
-    img = imgs[np.random.randint(1,1000)]
-    tab.image(img, width = 400)
+    tabs = st.tabs([f"Image {i+1}" for i in range(30)])
+    for tab in tabs:
+        img = imgs.sample().values[0]
+        tab.image(img, width=400)
